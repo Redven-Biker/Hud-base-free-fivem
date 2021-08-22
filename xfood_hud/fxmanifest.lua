f87b02b816f6b9bcc3bd476fe4c5d1ca97d46351
Redven-Biker <marween35620@gmail.com>
2021-08-22 05:39:14 +0200

    1.0

---
+++ xfood_hud/fxmanifest.lua
@@ -0,0 +1,26 @@
+  
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Redven Biker (XLife.fr)'
+description 'Discord : https://discord.gg/C3aN2bBVbB'
+version '1.0.0'
+
+ui_page 'html/ui.html'
+
+client_scripts {
+	'client.lua'
+}
+
+server_scripts {
+	'server.lua'
+}
+
+files {
+	-- Main Images
+	'html/ui.html',
+	'html/style.css',
+	'html/grid.css',
+	'html/main.js',
+	'html/img/background.jpg'
+}
---
+++ xfood_hud/html/main.js
@@ -0,0 +1,46 @@
+﻿
+var rgbStart = [139,195,74]
+var rgbEnd = [183,28,28]
+
+function updateWeight(weight){
+
+
+	var bgcolor = colourGradient(weight/100, rgbEnd, rgbStart)
+	$('#weight .bg').css('height', weight+'%')
+	$('#weight .bg').css('background-color', 'rgb(' + bgcolor[0] +','+ bgcolor[1] +','+ bgcolor[2] +')')
+}
+
+function updateCarStatus(status){
+	var gas = status[0]
+	$('#gas .bg').css('height', gas.percent+'%')
+	var bgcolor = colourGradient(gas.percent/100, rgbStart, rgbEnd)
+	//var bgcolor = colourGradient(0.1, rgbStart, rgbEnd)
+	//$('#gas .bg').css('height', '10%')
+	$('#gas .bg').css('background-color', 'rgb(' + bgcolor[0] +','+ bgcolor[1] +','+ bgcolor[2] +')')
+}
+
+function setValue(key, value){
+	$('#'+key+' span').html(value)
+
+}
+
+function updateStatus(status){
+	var hunger = status[0]
+	var thirst = status[1]
+	$('#hunger .bg').css('height', hunger.percent+'%')
+	$('#water .bg').css('height', thirst.percent+'%');
+
+}
+
+//API Shit
+function colourGradient(p, rgb_beginning, rgb_end){
+    var w = p * 2 - 1;
+
+    var w1 = (w + 1) / 2.0;
+    var w2 = 1 - w1;
+
+    var rgb = [parseInt(rgb_beginning[0] * w1 + rgb_end[0] * w2),
+        parseInt(rgb_beginning[1] * w1 + rgb_end[1] * w2),
+            parseInt(rgb_beginning[2] * w1 + rgb_end[2] * w2)];
+    return rgb;
+};
